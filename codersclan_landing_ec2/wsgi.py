d296b948cee44e5f8c1e0a5e478790dece06ea32
Rajasekhar <rajasekhar.cse537@gmail.com>
2024-11-21 14:31:56 +0530

    initial commit

---
+++ codersclan_landing_ec2/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for codersclan_landing_ec2 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codersclan_landing_ec2.settings')
+
+application = get_wsgi_application()
